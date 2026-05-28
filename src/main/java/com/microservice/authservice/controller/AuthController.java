836fb7955656e5a3eab8167e68de3d5b0134ccbf
ribasgb01 <ribasgb01@gmail.com>
2026-05-27 23:53:37 -0300

    Commit

--- src/main/java/com/microservice/authservice/controller/AuthController.java
+++ src/main/java/com/microservice/authservice/controller/AuthController.java
@@ -3,15 +3,16 @@ package com.microservice.authservice.controller;
 import com.microservice.authservice.dto.RegisterRequestDTO;
 import com.microservice.authservice.service.AuthService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/api/auth")
 public class AuthController {
 
     private AuthService authService;
-
 
     @PostMapping("/register")
     public ResponseEntity<String> register (@RequestBody RegisterRequestDTO body){
